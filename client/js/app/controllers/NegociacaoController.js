3542ae3ef5059a20cc68bdc0a251ddad0589905f
Renato Viegas <renato.viegas@softplan.com.br>
2020-06-08 01:49:36 -0300

    Aula 09 - Criando a classe Bind que fica responsável por encapsular o ProxyFactory e já manter a ligação entre Model e View

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,43 +6,20 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    const self = this;
-
-    this._negociacoes = new Proxy(new ListaNegociacoes(), {
-      get(target, prop, receiver) {
-        if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {
-          return function () {
-            console.log(`interceptando ${prop}`);
-
-            self._negociacoesView.update(target);
-
-            console.log(target);
-            Reflect.apply(target[prop], target, arguments);
-          }
-        }
-
-        return Reflect.get(target, prop, receiver);
-      }
-    });
-    this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
-    this._mensagem = new Mensagem();
-    this._mensagemView = new MensagemView($('#mensagemView'));
-    this._mensagemView.update(this._mensagem);
+    this._negociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia');
+    this._mensagem = new Bind(new Mensagem(), new MensagemView($('#mensagemView')), 'texto');
   }
 
   adiciona(event) {
     event.preventDefault();
     this._negociacoes.adiciona(this._criaNegociacao());
     this._mensagem.texto = 'Negociação adicionada com sucesso!';
-    this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
   }
 
   apaga() {
     this._negociacoes.esvazia();
     this._mensagem.texto = 'Negociações apagadas com sucesso!';
-    this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
   }
 
